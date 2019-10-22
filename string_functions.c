d7baba0b1cec4a4fb418e0e4d25bef12fb2e23b9
Nikita-Fast <N1kita.fast@yandex.ru>
2019-10-22 23:05:00 +0300

    fixed errors

--- string_functions.c
+++ string_functions.c
@@ -1,47 +1,42 @@
 #include <stdio.h>
 
-int strlen(char* pc) {
-    int length = 0;
-    int i = 0;
-    while(*(pc + i) != '\0') {
+size_t strlen(char* pc) {
+    size_t length = 0;
+    while (*(pc + length) != '\0') {
         length ++;
-        i ++;
     }
     return length;
 }
 
 void strcpy(char* dst, char* src) {
-    int i = 0;
-    while(*(src + i) != '\0') {
-        *(dst + i) = *(src + i);
-        i++;
+    while (*src != '\0') {
+       *dst = *src;
+        dst ++;
+        src ++;
     }
 }
 
 void strcat(char* dst, char* src) {
-    while(*dst != '\0') {
+    while (*dst != '\0') {
         dst ++;
     }
-    while(*src != '\0') {
-        *dst = *src;
+    while (*src != '\0') {
+       *dst = *src;
         dst ++;
         src ++;
     }
 }
 
 int strcmp(char* s1, char* s2) {
-    while((*s1 == *s2) & (*s1 != '\0')) {
+    while ((*s1 == *s2) && (*s1 != '\0')) {
         s1 ++;
         s2 ++;
     }
-    if(*s1 < *s2) {
+    if (*s1 < *s2) {
         return -1;
-    } else if(*s1 == *s2) {
+    } else if (*s1 == *s2) {
         return 0;
     } else {
         return 1;
     }
 }
-
-
-
