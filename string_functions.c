5cf0c679c198f21cc2adc39ed343c5ec4b95a9c4
Nikita-Fast <N1kita.fast@yandex.ru>
2019-10-26 21:31:46 +0300

    delete shifts before ++

--- string_functions.c
+++ string_functions.c
@@ -3,7 +3,7 @@
 size_t strlen(char* pc) {
     size_t length = 0;
     while (*(pc + length) != '\0') {
-        length ++;
+        length++;
     }
     return length;
 }
@@ -11,26 +11,26 @@ size_t strlen(char* pc) {
 void strcpy(char* dst, char* src) {
     while (*src != '\0') {
        *dst = *src;
-        dst ++;
-        src ++;
+        dst++;
+        src++;
     }
 }
 
 void strcat(char* dst, char* src) {
     while (*dst != '\0') {
-        dst ++;
+        dst++;
     }
     while (*src != '\0') {
        *dst = *src;
-        dst ++;
-        src ++;
+        dst++;
+        src++;
     }
 }
 
 int strcmp(char* s1, char* s2) {
     while ((*s1 == *s2) && (*s1 != '\0')) {
-        s1 ++;
-        s2 ++;
+        s1++;
+        s2++;
     }
     if (*s1 < *s2) {
         return -1;
@@ -40,3 +40,5 @@ int strcmp(char* s1, char* s2) {
         return 1;
     }
 }
+
+
