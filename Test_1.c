90df7b5b66c58beb5d822c648b0634a78b0f6815
Nikita-Fast <N1kita.fast@yandex.ru>
2019-10-27 12:53:56 +0300

    stringToNumber also works with negative integers

--- Test_1.c
+++ Test_1.c
@@ -14,37 +14,56 @@ int fibbonachiNumber(int n) {
     return result;
 }
 
+//situation "-   123" not considered a mistake, program will give result equal -123
 int stringToNumber(char string[]) {
     size_t length = strlen(string);
     size_t symbolError = 0;
     size_t spaceError = 0;
+    size_t minusError = 0;
+    size_t amountOfMinusesError = 0;
     size_t spaceFlag = 0;
     size_t numberFlag = 0;
     size_t firstDigitFlag = 0;
     size_t overflowFlag = 0;
+    size_t negativeFlag = 0;
+    size_t minusCounter = 0;
+    int minusNumber = 1;
+    int overflowConst = 214748364;
     int result = 0;
     for (size_t i = 0; i < length; i++) {
         if (string[i] >= '0' && string[i] <= '9') {
-            if (result > 214748364 || \
-                result == 214748364 && (string[i] == '8' || string[i] == '9')) {
+            if (result > overflowConst || result < -1 * overflowConst || \
+                result == 214748364 && (string[i] == '8' || string[i] == '9') || \
+                result == -1 * overflowConst && string[i] == '9') {
                 overflowFlag = 1;
             }
             if (firstDigitFlag) {
                 result *= 10;
             }
-            result += string[i] - '0';
+            result += (string[i] - '0') * minusNumber;
             firstDigitFlag = 1;
             numberFlag = 1;
         }
-        if ((numberFlag == 1) && (string[i] == 32)) {
+        if (numberFlag == 1 && string[i] == ' ') {
             spaceFlag = 1;
             numberFlag = 0;
         }
         if (numberFlag == 1 && spaceFlag == 1) {
             spaceError = 1;
         }
-        if (string[i] != 32 && (string[i] > '9' || string[i] < '0')) {
+        if (string[i] != ' ' && string[i] != '-' && (string[i] > '9' || string[i] < '0')) {
             symbolError = 1;
+        }
+        if (firstDigitFlag == 1 && string[i] == '-') {
+            minusError = 1;
+        }
+        if (minusCounter > 1) {
+            amountOfMinusesError = 1;
+        }
+        if (string[i] == '-') {
+            minusCounter++;
+            negativeFlag = 1;
+            minusNumber = -1;
         }
     }
     if (!length) {
@@ -56,10 +75,16 @@ int stringToNumber(char string[]) {
     if (symbolError) {
         printf("Error: string contain banned symbols\n");
     }
+    if (minusError) {
+        printf("Error: minus can't stay after digit\n");
+    }
+    if (amountOfMinusesError) {
+        printf("Error: string contain more than 1 minus\n");
+    }
     if (overflowFlag) {
         printf("Error: integer overflow detected\n");
     }
-    if (length && !spaceError && !symbolError && !overflowFlag) {
+    if (length && !spaceError && !symbolError && !overflowFlag && !minusError && !amountOfMinusesError) {
         return result;
     } else {
         return -1;
@@ -100,7 +125,7 @@ void numbers_order(void) {
 
 /*
 int main() {
-    char m[50] = "564833";
+    char m[50] = "-   123 ";
     printf("%i", stringToNumber(m));
 }
 */
