32c47cf7a52c3f0b76d66a94e98564773eaa6760
Nikita-Fast <N1kita.fast@yandex.ru>
2021-02-06 04:09:01 +0300

    obtain first results

---
+++ term3_hw2/src/main/java/com/company/Main.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2014, Oracle America, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of Oracle nor the names of its contributors may be used
+ *    to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.company;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Param;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.sql.Time;
+import java.util.ArrayList;
+
+public class Main {
+
+
+    public void testMethod() {
+        // This is a demo/sample template for building your JMH benchmarks. Edit as needed.
+        // Put your benchmark code here.
+    }
+
+
+
+    public static void main(String[] args) {
+        String basePath = new File("").getAbsolutePath();
+        int radius = 25;
+        double variance = 1.5;
+        GaussianBlur gaussianBlur = new GaussianBlur(radius, variance);
+        gaussianBlur.setSourceImage("kerry.jpg", basePath);
+        ArrayList<Thread> threads = new ArrayList<>();
+        int threadsNumber = 4;
+        long start = System.currentTimeMillis();
+
+        for (int i = 0; i < threadsNumber; i++) {
+            final int initValue = i;
+            Thread thread = new Thread(() -> {
+                int limit = gaussianBlur.getSourceImage().getHeight() - 1;
+                int y = initValue;
+                while (y <= limit) {
+                    gaussianBlur.createGaussianedImage(0, gaussianBlur.getSourceImage().getWidth() - 1, y, y);
+                    y += threadsNumber;
+                }
+            });
+            threads.add(thread);
+        }
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("time = " + (System.currentTimeMillis() - start) / 1000.0);
+
+        try {
+            ImageIO.write(gaussianBlur.getResultImage(), "PNG", new File("sample.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ term3_hw2/src/main/java/test/MyBenchmark.java
@@ -0,0 +1,134 @@
+package test;
+
+import com.company.GaussianBlur;
+import org.openjdk.jmh.annotations.*;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+@State(Scope.Thread)
+@Warmup(iterations = 3, time = 15)
+@Measurement(iterations = 3, time = 10)
+@Fork(3)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+public class MyBenchmark {
+
+    GaussianBlur gaussianBlur;
+
+    @Setup(Level.Trial)
+    public void prepare() {
+        String basePath = new File("").getAbsolutePath();
+        int radius = 25;
+        double variance = 1.5;
+        gaussianBlur = new GaussianBlur(radius, variance);
+        gaussianBlur.setSourceImage("kerry.jpg", basePath);
+    }
+
+    @Param({"32", "16", "8", "4", "2", "1"})
+    public int threadsNumber;
+
+    @Benchmark
+    public void isVertical() { //processed lines are vertical
+        ArrayList<Thread> threads = new ArrayList<>();
+
+        for (int i = 0; i < threadsNumber; i++) {
+            final int initValue = i;
+            Thread thread = new Thread(() -> {
+                int limit = gaussianBlur.getSourceImage().getWidth() - 1;
+                int x = initValue;
+                while (x <= limit) {
+                    gaussianBlur.createGaussianedImage(x, x, 0, gaussianBlur.getSourceImage().getHeight() - 1);
+                    x += threadsNumber;
+                }
+            });
+            threads.add(thread);
+        }
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Benchmark
+    public void isHorizontal() { //processed lines are horizontal
+        ArrayList<Thread> threads = new ArrayList<>();
+
+        for (int i = 0; i < threadsNumber; i++) {
+            final int initValue = i;
+            Thread thread = new Thread(() -> {
+                int limit = gaussianBlur.getSourceImage().getHeight() - 1;
+                int y = initValue;
+                while (y <= limit) {
+                    gaussianBlur.createGaussianedImage(0, gaussianBlur.getSourceImage().getWidth() - 1, y, y);
+                    y += threadsNumber;
+                }
+            });
+            threads.add(thread);
+        }
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
+    @TearDown(Level.Invocation)
+    public void saveAndCheckResult() {
+        try {
+            ImageIO.write(gaussianBlur.getResultImage(), "PNG", new File("resultJMH.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (!imagesAreEqual("resultJMH.png", "sample.png")) {
+            System.exit(1091);
+        }
+    }
+
+    public boolean imagesAreEqual(String name1, String name2) {
+        BufferedImage im1 = null;
+        BufferedImage im2 = null;
+
+        try {
+            im1 = ImageIO.read(new File(new File("").getAbsolutePath() + "/" + name1));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            im2 = ImageIO.read(new File(new File("").getAbsolutePath() + "/" + name2));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (im1.getWidth() != im2.getWidth() || im1.getHeight() != im2.getHeight()) {
+            return false;
+        }
+        for (int x = 0; x < im1.getWidth(); x++) {
+            for (int y = 0; y < im1.getHeight(); y++) {
+                if (im1.getRGB(x, y) != im2.getRGB(x, y)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
