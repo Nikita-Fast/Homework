2b76b38cec7cdc30d3df380adde5d10c9d64f2e3
Nikita-Fast <N1kita.fast@yandex.ru>
2021-06-03 09:25:51 +0300

    good working version of code

---
+++ term3_hw3_2/src/main/java/com/company/operations/AssociativeOperation.java
@@ -0,0 +1,1 @@
+package com.company.operations;public abstract class AssociativeOperation<T> {    public abstract T apply(T x, T y);}
---
+++ term3_hw3_2/src/main/java/com/company/operations/AssociativeOperationWithNeutralElement.java
@@ -0,0 +1,1 @@
+package com.company.operations;public abstract class AssociativeOperationWithNeutralElement<T> extends AssociativeOperation<T> {    private T neutralElement;    public void setNeutralElement(T neutralElement) {        this.neutralElement = neutralElement;    }    public T getNeutralElement() {        return neutralElement;    }}
