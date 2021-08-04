2a66ceb6d9427f674bfe4f4607e796e78fbf8a02
Nikita-Fast <N1kita.fast@yandex.ru>
2021-08-04 23:13:25 +0300

    remove garbage

--- term3_hw4/src/com/company/Coefficients.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Coefficients {
-    private double[] coefficients;
-
-    public Coefficients(double[] coefficients) {
-        this.coefficients = coefficients;
-    }
-}
